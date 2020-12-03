5d1e41f38ef3629d73999abae38b4c95424617ec
Matei Oltean <matei.oltean@gmail.com>
2020-12-03 20:20:51 +0100

    Add solution for day 01

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # adventofcode2020
+
+C++ solutions for the Advent of Code 2020.
+
+## Build and Run
+
+On Windows with MinGW:
+
+```powershell
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" ..
+mingw32-make.exe
+```
+
+Then run the executable that you need:
+
+```powershell
+cat ..\dayxx\input | .\dayxx\dayxx.exe
+```
---
+++ day01/main.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+uint64_t sol1() {
+    unordered_set<uint64_t> d;
+    string line;
+    while (getline(cin, line)) {
+        auto num = stoull(line);
+        if (d.count(2020-num)) {
+            return num*(2020-num);
+        }
+        d.emplace(num);
+    }
+}
+
+uint64_t sol2() {
+    unordered_set<uint64_t> num;
+    unordered_map<uint64_t, pair<uint64_t, uint64_t>> d;
+    string line;
+    while (getline(cin, line)) {
+        auto a = stoull(line);
+        if (d.count(2020-a)) {
+            auto res = d.at(2020-a);
+            return a*res.first*res.second;
+        }
+        for (auto k : num) {
+            d.emplace(k+a, make_pair(k, a));
+        }
+        num.emplace(a);
+    }
+}
+
+int main() {
+    cout << sol2() << endl;
+}
