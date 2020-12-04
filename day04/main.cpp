7b7c77779c3dadb34ab3eb877982ec83ff061b42
Matei Oltean <matei.oltean@gmail.com>
2020-12-04 09:56:54 +0100

    Add day04.2

--- day04/main.cpp
+++ day04/main.cpp
@@ -2,10 +2,63 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <iostream>
+#include <functional>
+#include <regex>
 
 using namespace std;
 
-const unordered_set<string> mandatory_keys { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+const unordered_map<string, function<bool(string)> > mandatory_keys {
+    { "byr", [](auto a) {
+        regex r ("^[1-2][0-9]{3}$");
+        if (!regex_match(a, r))
+            return false;
+        auto num = stoull(a);
+        return (1920 <= num) && (num <= 2002);
+     }
+    },
+    { "iyr", [](auto a) {
+        regex r ("^[1-2][0-9]{3}$");
+        if (!regex_match(a, r))
+            return false;
+        auto num = stoull(a);
+        return (2010 <= num) && (num <= 2020);
+     }
+    },
+    { "eyr", [](auto a) {
+        regex r ("^[1-2][0-9]{3}$");
+        if (!regex_match(a, r))
+            return false;
+        auto num = stoull(a);
+        return (2020 <= num) && (num <= 2030);
+     }
+    },
+    { "hgt", [](auto a) {
+        regex r ("^[1-9][0-9]*(cm|in)$");
+        if (!regex_match(a, r))
+            return false;
+        auto h = a.substr(0, a.length() - 2);
+        auto height = stoull(h);
+        if (a.at(a.length() - 1) == 'n')
+            return (59 <= height) && (height <= 76);
+        return (150 <= height) && (height <= 193);
+     }
+    },
+    { "hcl", [](auto a) {
+        regex r ("^#[0-9a-f]{6}$");
+        return regex_match(a, r);
+     }
+    },
+    { "ecl", [](auto a) {
+        regex r ("^(amb|blu|brn|gry|grn|hzl|oth)$");
+        return regex_match(a, r);
+     }
+    },
+    { "pid", [](auto a) {
+        regex r ("^[0-9]{9}$");
+        return regex_match(a, r);
+     }
+    }
+};
 
 uint64_t sol1() {
     auto result = 0;
@@ -15,7 +68,7 @@ uint64_t sol1() {
         if (line.empty()) {
             auto valid = true;
             for (auto key: mandatory_keys) {
-                if (!fields.count(key)) {
+                if (!fields.count(key.first)) {
                     valid = false;
                     break;
                 }
@@ -39,7 +92,38 @@ uint64_t sol1() {
     return result;
 }
 
+uint64_t sol2() {
+    auto result = 0;
+    string line;
+    unordered_map<string, string> fields;
+    while (getline(cin, line)) {
+        if (line.empty()) {
+            auto valid = true;
+            for (auto key: mandatory_keys) {
+                if (!fields.count(key.first) || !key.second(fields.at(key.first))) {
+                    valid = false;
+                    break;
+                }
+            }
+            result += valid;
+            fields.clear();
+            continue;
+        }
+        size_t start = 0, end = 0;
+        while (end != string::npos) {
+            end = line.find_first_of(" ", start);
+            auto key_value = line.substr(start, end - start);
+            auto semi_colon_pos = key_value.find_first_of(":");
+            auto key = key_value.substr(0, semi_colon_pos);
+            auto value = key_value.substr(semi_colon_pos + 1, string::npos);
+            fields.emplace(key, value);
+            start = end + 1;
+        }
+    }
+    return result;
+}
+
 
 int main() {
-    cout << sol1() << endl;
+    cout << sol2() << endl;
 }
