c69848fa89d37a4d9318af892e4b72ceb3bf9273
Matei Oltean <matei.oltean@gmail.com>
2020-12-05 09:00:43 +0100

    Add day05

---
+++ day05/main.cpp
@@ -0,0 +1,84 @@
+#include <string>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <./utils.h>
+
+using namespace std;
+
+int ID(const pair<int, int>& seat) {
+    return 8 * seat.first + seat.second;
+}
+
+uint64_t sol1() {
+    auto result = 0;
+    string line;
+    int mid;
+    while (getline(cin, line)) {
+        int low = 0, high = 127;
+        for (int i = 0; i < 7; ++i) {
+            mid = (low + high)/2;
+            if (line.at(i) == 'F') {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int left = 0, right = 7;
+        for (int i = 7; i < 10; ++i) {
+            mid = (left + right)/2;
+            if (line.at(i) == 'L') {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        result = max(result, ID({ low, left }));
+    }
+    return result;
+}
+
+uint64_t sol2() {
+    string line;
+    int minID = ID({ 127, 7 }), mid;
+    set<pair<int, int>> seats;
+    while (getline(cin, line)) {
+        int low = 0, high = 127;
+        for (int i = 0; i < 7; ++i) {
+            mid = (low + high)/2;
+            if (line.at(i) == 'F') {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int left = 0, right = 7;
+        for (int i = 7; i < 10; ++i) {
+            mid = (left + right)/2;
+            if (line.at(i) == 'L') {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        minID = min(minID, ID({ low, left }));
+        seats.emplace(low, left);
+    }
+    for (int i = 0; i < 127; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            int id = ID({ i, j });
+            if (id <= minID) {
+                break;
+            }
+            if (!seats.count({ i, j })) {
+                return id;
+            }
+        }
+    }
+    return 0;
+}
+
+
+int main() {
+    cout << sol2() << endl;
+}
