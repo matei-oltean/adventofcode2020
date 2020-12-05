5fe8e21cb18f19e1688731b184970bbfb6b37a7b
Matei Oltean <matei.oltean@gmail.com>
2020-12-05 13:15:18 +0100

    day05: Factorise code

--- day05/main.cpp
+++ day05/main.cpp
@@ -6,74 +6,35 @@
 
 using namespace std;
 
-int ID(const pair<int, int>& seat) {
-    return 8 * seat.first + seat.second;
+int ID(string& seat) {
+    replace(seat.begin(), seat.end(), 'B', '1');
+    replace(seat.begin(), seat.end(), 'F', '0');
+    replace(seat.begin(), seat.end(), 'R', '1');
+    replace(seat.begin(), seat.end(), 'L', '0');
+    return stoi(seat, nullptr, 2);
 }
 
 uint64_t sol1() {
     auto result = 0;
     string line;
-    int mid;
     while (getline(cin, line)) {
-        int low = 0, high = 127;
-        for (int i = 0; i < 7; ++i) {
-            mid = (low + high)/2;
-            if (line.at(i) == 'F') {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        int left = 0, right = 7;
-        for (int i = 7; i < 10; ++i) {
-            mid = (left + right)/2;
-            if (line.at(i) == 'L') {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        result = max(result, ID({ low, left }));
+        result = max(result, ID(line));
     }
     return result;
 }
 
 uint64_t sol2() {
     string line;
-    int minID = ID({ 127, 7 }), mid;
+    int minID = 1024;
     unordered_set<int> seats;
     while (getline(cin, line)) {
-        int low = 0, high = 127;
-        for (int i = 0; i < 7; ++i) {
-            mid = (low + high)/2;
-            if (line.at(i) == 'F') {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        int left = 0, right = 7;
-        for (int i = 7; i < 10; ++i) {
-            mid = (left + right)/2;
-            if (line.at(i) == 'L') {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        int id = ID({ low, left });
+        int id = ID(line);
         minID = min(minID, id);
         seats.emplace(id);
     }
-    for (int i = 0; i < 127; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            int id = ID({ i, j });
-            if (id <= minID) {
-                break;
-            }
-            if (!seats.count(id)) {
-                return id;
-            }
+    for (; minID < 1024; ++minID) {
+        if (!seats.count(minID)) {
+            return minID;
         }
     }
     return 0;
