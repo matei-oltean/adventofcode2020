ce9378f8470131d585119d9e396e572be2fb8bcf
Matei Oltean <matei.oltean@gmail.com>
2020-12-17 12:37:40 +0100

    day17: use unordered_map instead of map

--- day17/main.cpp
+++ day17/main.cpp
@@ -1,31 +1,38 @@
 #include <string>
 #include <iostream>
 #include <set>
-#include <map>
+#include <unordered_map>
 #include <tuple>
 
 using namespace std;
 using point = tuple<int, int, int>;
 using point4d = tuple<int, int, int, int>;
+using map3d = unordered_map<int, unordered_map<int, unordered_map<int, int> > >;
+using map4d = unordered_map<int, map3d>;
 
 void run(set<point>& active, size_t iterations) {
     for (size_t iter = 0; iter < iterations; ++iter) {
         set<point> new_active;
-        map<point, int> neighbours;
+        map3d neighbours;
         for (const auto& pt : active) {
             const auto& [x, y, z] = pt;
-            neighbours[pt] -= 1;
+            neighbours[x][y][z] -= 1;
             for (int xx = x - 1; xx <= x + 1; ++xx) {
                 for (int yy = y - 1; yy <= y + 1; ++yy) {
                     for (int zz = z - 1; zz <= z + 1; ++zz) {
-                        neighbours[make_tuple(xx, yy, zz)] += 1;
+                        neighbours[xx][yy][zz] += 1;
                     }
                 }
             }
         }
-        for (const auto& cube : neighbours) {
-            if (cube.second == 3 || (cube.second == 2 && active.count(cube.first))) {
-                new_active.emplace(cube.first);
+        for (const auto& x : neighbours) {
+            for (const auto& y : x.second) {
+                for (const auto& z : y.second) {
+                    const point p = make_tuple(x.first, y.first, z.first);
+                    if (z.second == 3 || (z.second == 2 && active.count(p))) {
+                        new_active.emplace(p);
+                    }
+                }
             }
         }
         active = new_active;
@@ -35,23 +42,30 @@ void run(set<point>& active, size_t iterations) {
 void run(set<point4d>& active, size_t iterations) {
     for (size_t iter = 0; iter < iterations; ++iter) {
         set<point4d> new_active;
-        map<point4d, int> neighbours;
+        map4d neighbours;
         for (const auto& pt : active) {
             const auto& [x, y, z, w] = pt;
-            neighbours[pt] -= 1;
+            neighbours[x][y][z][w] -= 1;
             for (int xx = x - 1; xx <= x + 1; ++xx) {
                 for (int yy = y - 1; yy <= y + 1; ++yy) {
                     for (int zz = z - 1; zz <= z + 1; ++zz) {
                         for (int ww = w - 1; ww <= w + 1; ++ww) {
-                            neighbours[make_tuple(xx, yy, zz, ww)] += 1;
+                            neighbours[xx][yy][zz][ww] += 1;
                         }
                     }
                 }
             }
         }
-        for (const auto& cube : neighbours) {
-            if (cube.second == 3 || (cube.second == 2 && active.count(cube.first))) {
-                new_active.emplace(cube.first);
+        for (const auto& x : neighbours) {
+            for (const auto& y : x.second) {
+                for (const auto& z : y.second) {
+                    for (const auto& cube : z.second) {
+                        const point4d p = make_tuple(x.first, y.first, z.first, cube.first);
+                        if (cube.second == 3 || (cube.second == 2 && active.count(p))) {
+                            new_active.emplace(p);
+                        }
+                    }
+                }
             }
         }
         active = new_active;
